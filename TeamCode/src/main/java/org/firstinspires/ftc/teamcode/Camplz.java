a0eb31360fa885ef1f9e7dfb98fe20bf8446ce48
anonamause10 <anonamause10@gmail.com>
2019-10-13 22:17:59 -0500

    Got some CV code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Camplz.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Camplz.java
@@ -22,22 +22,45 @@
 package org.firstinspires.ftc.teamcode;
 
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera;
 import org.openftc.easyopencv.OpenCvPipeline;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @TeleOp
 public class Camplz extends LinearOpMode
 {
     OpenCvCamera phoneCam;
+    private Mat displayMat = new Mat(); // Display debug info to the screen (this is what is returned)
+    private Mat workingMat = new Mat(); // Used for preprocessing and working with (blurring as an example)
+    private Mat mask       = new Mat(); // Mask returned by color filter
+    private Mat hierarchy  = new Mat(); // hierarchy used by coutnours
+    private boolean found    = false; // Is the gold mineral found
+    private Point   screenPosition = new Point(); // Screen position of the mineral
+    private Rect    foundRect = new Rect(); // Found rect
+    private List<Mat> channels = new ArrayList<>();
+    private double perfectRatio = 0.33;
+    private Size adjustedSize;
+    public Size   downscaleResolution = new Size(320, 240);
+    public boolean useFixedDownscale = true;
+    private Size initSize;
+    public double downscale = 0.25;
 
     @Override
     public void runOpMode()
@@ -79,6 +102,7 @@ public class Camplz extends LinearOpMode
          * For a rear facing camera or a webcam, rotation is defined assuming the camera is facing
          * away from the user.
          */
+
         phoneCam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
 
         /*
@@ -155,7 +179,7 @@ public class Camplz extends LinearOpMode
              * excess CPU cycles for no reason. (By default, telemetry is only sent to the DS at 4Hz
              * anyway). Of course in a real OpMode you will likely not want to do this.
              */
-            sleep(100);
+
         }
     }
 
@@ -188,6 +212,7 @@ public class Camplz extends LinearOpMode
         @Override
         public Mat processFrame(Mat input)
         {
+            Log.w("sizes","Input size: "+input.depth());
             /*
              * IMPORTANT NOTE: the input Mat that is passed in as a parameter to this method
              * will only dereference to the same image for the duration of this particular
@@ -199,23 +224,106 @@ public class Camplz extends LinearOpMode
             /*
              * Draw a simple box around the middle 1/2 of the entire frame
              */
-            Imgproc.rectangle(
-                    input,
-                    new Point(
-                            input.cols()/4,
-                            input.rows()/4),
-                    new Point(
-                            input.cols()*(3f/4f),
-                            input.rows()*(3f/4f)),
-                    new Scalar(0, 255, 0), 4);
+            initSize = input.size();
 
-            /**
-             * NOTE: to see how to get data from your pipeline to your OpMode as well as how
-             * to change which stage of the pipeline is rendered to the viewport when it is
-             * tapped, please see {@link PipelineStageSwitchingExample}
-             */
+            if(useFixedDownscale){
+                adjustedSize = downscaleResolution;
+            }else{
+                adjustedSize = new Size(initSize.width * downscale, initSize.height * downscale);
+            }
+            input.copyTo(displayMat);
+            input.copyTo(workingMat);
 
-            return input;
+            Imgproc.GaussianBlur(workingMat,workingMat,new Size(5,5),0);
+            Mat colormat = workingMat.clone();
+            channels = new ArrayList<>();
+
+            Imgproc.cvtColor(colormat, colormat, Imgproc.COLOR_RGB2Lab);
+            Imgproc.GaussianBlur(colormat,colormat,new Size(3,3),0);
+            Core.split(colormat, channels);
+            if(channels.size() > 0){
+                Imgproc.threshold(channels.get(1), mask, 164, 255, Imgproc.THRESH_BINARY);
+            }
+
+
+            for(int i=0;i<channels.size();i++){
+                channels.get(i).release();
+            }
+
+            colormat.release();
+
+            List<MatOfPoint> contoursYellow = new ArrayList<>();
+            Imgproc.findContours(mask      , contoursYellow, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+                Imgproc.drawContours(displayMat,contoursYellow,-1,new Scalar(230,70,70),2);
+            Rect bestRect = null;
+            double bestDiffrence = Double.MAX_VALUE; // MAX_VALUE since less diffrence = better
+            for(MatOfPoint cont : contoursYellow){
+                double score = calculateScore(cont); // Get the diffrence score using the scoring API
+
+                // Get bounding rect of contour
+                Rect rect = Imgproc.boundingRect(cont);
+                Imgproc.rectangle(displayMat, rect.tl(), rect.br(), new Scalar(0,0,255),2); // Draw rect
+
+                // If the result is better then the previously tracked one, set this rect as the new best
+                if(score < bestDiffrence){
+                    bestDiffrence = score;
+                    bestRect = rect;
+                }
+            }
+            if(bestRect != null){
+                // Show chosen result
+                Imgproc.rectangle(displayMat, bestRect.tl(), bestRect.br(), new Scalar(255,0,0),4);
+                Imgproc.putText(displayMat, "Chosen", bestRect.tl(),0,1,new Scalar(255,255,255));
+
+                screenPosition = new Point(bestRect.x, bestRect.y);
+                foundRect = bestRect;
+                found = true;
+            }else{
+                found = false;
+            }
+
+
+            //Print result
+            Imgproc.putText(displayMat,"Result: " + screenPosition.x +"/"+screenPosition.y,new Point(10,getAdjustedSize().height-30),0,1, new Scalar(255,255,0),1);
+
+            Log.w("sizes","Display size: "+displayMat.depth());
+
+            return displayMat;
+
+
         }
+
+        public double calculateScore(Mat input){
+            double totalScore = 0;
+
+            if(!(input instanceof MatOfPoint)) return Double.MAX_VALUE;
+            MatOfPoint contour = (MatOfPoint) input;
+            double score = Double.MAX_VALUE;
+
+            // Get bounding rect of contour
+            Rect rect = Imgproc.boundingRect(contour);
+            double x = rect.x;
+            double y = rect.y;
+            double w = rect.width;
+            double h = rect.height;
+
+            double cubeRatio = Math.max(Math.abs(h/w), Math.abs(w/h)); // Get the ratio. We use max in case h and w get swapped??? it happens when u account for rotation
+            double ratioDiffrence = Math.abs(cubeRatio - perfectRatio);
+            double ratioscore = ratioDiffrence*3;
+
+            MatOfPoint contourA = (MatOfPoint) input;
+            double area = Imgproc.contourArea(contourA);
+
+            double areascore = -area;
+            totalScore += ratioscore;
+            totalScore += areascore;
+            return totalScore;
+        }
+
+        public Size getAdjustedSize() {
+            return adjustedSize;
+        }
+
+
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cvstuff/CV.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.CVstuff;
+
+
+public class CV {
+    public enum DetectionSpeed {
+        VERY_FAST,
+        FAST,
+        BALANCED,
+        SLOW,
+        VERY_SLOW
+    }
+
+    public enum AreaScoringMethod {
+        MAX_AREA,
+        PERFECT_AREA,
+        COLOR_DEVIATION
+    }
+
+    public enum CameraMode {
+        BACK,
+        FRONT,
+        WEBCAM
+    }
+}
+
