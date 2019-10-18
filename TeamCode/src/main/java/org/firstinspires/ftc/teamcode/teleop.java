2205b6a3ac34283150db942191cee68eab6fc5dd
anonamause10 <anonamause10@gmail.com>
2019-10-18 14:24:45 -0500

    easier to be black, squared joystick turning like was supposed to be

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
@@ -225,9 +225,12 @@ public class teleop extends LinearOpMode {
     private void drive(){
         //DONT TOUCH THIS
 
-        double r = Math.hypot(Math.pow(gamepad1.left_stick_x,2), Math.pow(gamepad1.left_stick_y,2));
-        double robotAngle = Math.atan2(-Math.pow(gamepad1.left_stick_y,2), Math.pow(gamepad1.left_stick_x,2)) - Math.PI / 4;
-        double rightX = gamepad1.right_stick_x;
+        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+        double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+        double rightX = Math.pow(gamepad1.right_stick_x,2);
+        if(gamepad1.right_stick_x<0){
+            rightX *= -1;
+        }
         double v1 = r * Math.cos(robotAngle) + rightX;
         double v2 = r * Math.sin(robotAngle) - rightX;
         double v3 = r * Math.sin(robotAngle) + rightX;
